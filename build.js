4622cd486c6458d1f1431d382846428f5553c1c4
Bernat Romagosa <bromagosa@e-citilab.eu>
2021-06-02 11:10:16 +0200

    done moving to handlebars, except for loops

--- build.js
+++ build.js
@@ -1,36 +1,38 @@
-var fs = require('fs');
-var hbs = require('hbs');
-var debugMode = true;
+var fs = require('fs'),
+    handlebars = require('handlebars'),
+    httpServer = require('http-server'),
+    debugMode = true;
 
 function debug (string) { if (debugMode) { console.info(string); } };
 
 function doForFilesInDir (dir, extension, action) {
     // does something for each file of a particular extension in a directory
-    var path = __dirname + dir;
+    var path = `${__dirname}/${dir}`;
     var filenames = fs.readdirSync(path);
     filenames.forEach((filename) => {
         var matches =
-            new RegExp('^([^.]+).' + extension + '$').exec(filename);
+            new RegExp(`^([^.]+).${extension}$`).exec(filename);
         if (!matches) { return; }
         var fileName = matches[1];
-        var fileContents = fs.readFileSync(path + '/' + filename, 'utf8');
-        action.call(this, fileName, fileContents);
+        var fullPath = path + '/' + filename;
+        var fileContents = fs.readFileSync(fullPath, 'utf8');
+        action.call(this, fileName, fileContents, fullPath);
     });
 };
 
 function registerPartials (dir) {
     // registers handlebars partials in a particular templates/partials subdir
     doForFilesInDir(
-        '/src/templates/partials/' + (dir || ''),
+        `src/templates/partials/${dir || ''}`,
         'hbs',
-        function (fileName, fileContents) {
+        (fileName, fileContents) => {
             // if there's a dir, register the partial as dir.name
             // i.e. svg.icon-plus.svg
-            hbs.registerPartial(
+            handlebars.registerPartial(
                 (dir ? dir + '.' : '') + fileName,
                 fileContents
             );
-            debug('registered partial: ' + fileName);
+            debug(`registered partial: ${fileName}`);
         }
     );
 };
@@ -38,16 +40,16 @@ function registerPartials (dir) {
 function compileAll () {
     // compiles all templates
     doForFilesInDir(
-        '/src/templates/',
+        'src/templates',
         'hbs',
-        function (fileName, fileContents) {
+        (fileName, fileContents) => {
             fs.writeFileSync(
-                'dist/' + fileName + '.html',
-                hbs.compile(fileContents)({})
+                `dist/${fileName}.html`,
+                handlebars.compile(fileContents)({})
             );
-            debug('compiled template: ' + fileName);
+            debug(`compiled template: ${fileName}`);
         }
-    )
+    );
 };
 
 function build () {
@@ -55,10 +57,30 @@ function build () {
 
     // register all handlebars partials
     registerPartials('svg');
+    registerPartials('layouts');
     registerPartials();
 
     // compile all templates
     compileAll();
 };
 
+function watchDir (dir, action) {
+    doForFilesInDir(
+        dir,
+        'hbs',
+        (fileName, fileContents, fullPath) => {
+            debug(`watching file: ${fullPath}`);
+            fs.watchFile(fullPath, { interval: 1000 }, (c, p) => {
+                debug(`file ${fileName} in ${fullPath} changed. Rebuilding`);
+                fs.writeFileSync(
+                    `dist/${fileName}.html`,
+                    handlebars.compile(fs.readFileSync(fullPath, 'utf8'))({})
+                );
+            })
+        }
+    );
+};
+
 build();
+watchDir('src/templates');
+httpServer.createServer().listen(3000);
