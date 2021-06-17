81b7ad3ddf37fbaf49baf1ae5a536b6a2bd3fac1
Bernat Romagosa <bromagosa@e-citilab.eu>
2021-06-17 09:40:09 +0200

    tweaks to Rosza speech bubble

--- src/scripts/rosza.js
+++ src/scripts/rosza.js
@@ -4,20 +4,25 @@
 
 function roszaInit() {
 
-    let logo = document.querySelector('.header__logo');
-    let rosza = document.querySelector('.header__rosza');
+    let logo = document.querySelector('.header__logo'),
+        rosza = document.querySelector('.header__rosza'),
+        hovering = false,
+        timeout;
 
     logo.addEventListener('mouseenter', function() {
-
-        setTimeout(function() {
-            rosza.style.visibility = 'visible';
-            rosza.style.opacity = '1';
+        hovering = true;
+        timeout = setTimeout(function() {
+            if (hovering) {
+                rosza.style.visibility = 'visible';
+                rosza.style.opacity = '1';
+            }
         }, 2000);
     });
 
     logo.addEventListener('mouseleave', function() {
-
+        hovering = false;
         rosza.style.opacity = '0';
+        clearTimeout(timeout);
         setTimeout(function() {
             rosza.style.visibility = 'hidden';            
         }, 300); // CSS animation timing
